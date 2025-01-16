4e22d574f913cbe4156057d892ba58cffc21319a
Patrick Matthew <patrikksouza04@gmail.com>
2025-01-15 22:52:54 -0300

    Update repet_txt.py

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,11 @@
-# Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+# Agora vamos solicitar uma string e um número inteiro como entrada. Depois teremos que retornar a string repetida o número de vezes informado.
+
+string = input("Digite uma string: ")
+numero = int(input("Digite um número inteiro: "))
+
+print((string + ' ') * numero)
+
+# Exemplo de execução:
+# Digite uma string: Hello
+# Digite um número inteiro: 3
+# Hello! Hello! Hello!
